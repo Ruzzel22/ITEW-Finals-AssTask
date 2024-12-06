d81ddf093453deaff2195f0979f6d3f7889355d0
Ruzzel22 <ruzzelmolinyawe@gmail.com>
2024-12-06 10:54:10 +0800

    Add files via upload

--- product-script.js
+++ product-script.js
@@ -1,4 +1,58 @@
-// View More
+/* Product Preview */
+// Display preview
+document.querySelectorAll('.product img').forEach(img => {
+    img.addEventListener('click', (e) => {
+        const productPreview = document.getElementById('product-preview');
+        const previewGif = document.getElementById('preview-gif');
+
+        // alt Attribute clicked
+        const clickedProduct = e.target.alt;
+        // Condition when clicked
+        if (clickedProduct === 'Arknights') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Attack On Titan') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Azur Lane') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Bleach') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Boruto') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Dragon Ball') {
+            previewGif.src = 'img/Goku Vs Jiren.gif';
+        } else if (clickedProduct === 'Fairy Tail') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Katana Maidens') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'One Piece') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Oshi No Ko') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Seven Deadly Sins') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Shangri-La Frontier') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'The Eminence In Shadow') {
+            previewGif.src = 'img/@.gif';
+        } else if (clickedProduct === 'Undead Unluck') {
+            previewGif.src = 'img/@.gif';
+        }
+
+        productPreview.style.display = 'flex';
+        document.body.style.backgroundColor = 'transparent';
+    });
+});
+// Hide preview
+document.getElementById('product-preview').addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        const productPreview = document.getElementById('product-preview');
+        productPreview.style.display = 'none';
+        document.body.style.backgroundColor = '';
+    }
+});
+
+
+/* View More */
 // Display Product
 document.getElementById("view-more").addEventListener("click", function() {
     const hiddenItems = document.querySelectorAll(".product.hidden");
@@ -28,3 +82,9 @@ document.getElementById("view-less").addEventListener("click", function() {
     const button = document.getElementById('view-more');
     button.style.display = "block";
 });
+
+// Play Audio
+function playPaimonVoice() {
+    const paimonVoice = document.getElementById('voicePaimon');
+    paimonVoice.play();
+}
--- table-script.js
+++ table-script.js
@@ -1,10 +1,10 @@
 /* Table Static Data Implement */
 const tbody = document.querySelector("tbody");
-const template = document.querySelector("#table-row");
+const template = document.querySelector("#table-column");
 
-// Insert table data in Row
-const insertRow = template.content.cloneNode(true);
-let td = insertRow.querySelectorAll("td");
+// Insert table data in Column
+const insertColumn = template.content.cloneNode(true);
+let td = insertColumn.querySelectorAll("td");
 td[0].textContent = "MP4";
 td[1].textContent = "25GB";
 td[2].textContent = "16:9";
@@ -13,4 +13,4 @@ td[4].textContent = "AAC, 130 Kbps, 48000 Hz";
 td[5].textContent = "25 Minutes (Maximum)";
 td[6].innerHTML += "55 Hours (Minimum)";
 
-tbody.appendChild(insertRow);
+tbody.appendChild(insertColumn);
